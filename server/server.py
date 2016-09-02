c0118188e0e80848e95ba328758188754b166f32
Josh Myer <josh@joshisanerd.com>
2016-09-01 20:15:16 -0700

    Adds basic/janky websocket server to relay status from split.py out to the webclients

---
+++ server/server.py
@@ -0,0 +1,130 @@
+import json
+import logging
+
+from tornado import ioloop, web, websocket
+
+class PostHandler(web.RequestHandler):
+    def get(self, args):
+        print "get(%s)" % args
+
+        froms, tos = args.split("/")
+
+        msg = { "type": "click", "target2": tos, "sender": froms }
+
+        self.write( str(args) )
+        self.__send_to_connections(msg)
+
+
+
+
+    def __send_to_connections(self, msg):
+        if 'target' in msg:
+            self.application.log.debug('sending from %s to %s: %s',
+                    msg.get('sender', None), msg['target'], msg)
+
+            self.application.connections[msg['target']].write_message(
+                    json.dumps(msg))
+
+        else:
+            self.application.log.debug('sending from %s to everyone: %s',
+                    msg.get('sender', None), msg)
+
+            for each_connection in self.application.connections.itervalues():
+                each_connection.write_message(json.dumps(msg))
+
+
+
+class WebSocketServer(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        self.application.i += 1
+        self.connection_id = self.application.i
+        self.application.connections[self.connection_id] = self
+
+        # Connected event
+        self.write_message(json.dumps({
+                'type': 'connected',
+                'id': self.connection_id,
+                }))
+
+        # Connection events
+        for each_id, each_connection in self.application.connections.iteritems():
+
+            # Notify all clients of new connection
+            each_connection.write_message(json.dumps({
+                    'type': 'connection',
+                    'id': self.connection_id,
+                    'clients': len(self.application.connections),
+                    }))
+
+            # Notify current client of all existing connections
+            if each_id < self.connection_id:
+                self.write_message(json.dumps({
+                        'type': 'connection',
+                        'id': each_id,
+                        'clients': len(self.application.connections),
+                        }))
+
+    def on_message(self, message):
+        try:
+            msg = json.loads(message)
+            msg['sender'] = self.connection_id
+            self.__send_to_connections(msg)
+
+        except Exception, e:
+            self.write_message(json.dumps({
+                'type': 'error',
+                'error': str(e),
+                'received': message,
+                }))
+
+    def on_close(self):
+        self.application.log.debug('deleting %d', self.connection_id)
+
+        del self.application.connections[self.connection_id]
+
+        self.__send_to_connections({
+                'type': 'closed',
+                'id': self.connection_id,
+                'clients': len(self.application.connections),
+                })
+        pass
+
+    def __send_to_connections(self, msg):
+        if 'target' in msg:
+            self.application.log.debug('sending from %s to %s: %s',
+                    msg.get('sender', None), msg['target'], msg)
+
+            self.application.connections[msg['target']].write_message(
+                    json.dumps(msg))
+
+        else:
+            self.application.log.debug('sending from %s to everyone: %s',
+                    msg.get('sender', None), msg)
+
+            for each_connection in self.application.connections.itervalues():
+                each_connection.write_message(json.dumps(msg))
+
+
+if __name__ == '__main__':
+    application = web.Application([
+            (r'/post/(?P<args>.*)', PostHandler),
+            (r'/', WebSocketServer),
+            ])
+
+    application.i = 0
+    application.connections = {}
+
+    # Set up logging
+    handler = logging.StreamHandler()
+    handler.setFormatter(
+            logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+    application.log = logging.getLogger('websockets-demo')
+    application.log.setLevel(logging.DEBUG)
+    application.log.addHandler(handler)
+
+    application.listen(8081)
+    ioloop.IOLoop.instance().start()
