d42b004ba8ab42299972bcc538c9a460125b1cb1
Josh Myer <josh@joshisanerd.com>
2016-09-03 11:53:59 -0700

    Adds control channel decode, which entailed a non-trivial refactor of the codebase.  Still needs to be piped out to the webclient.

---
+++ scanner/smartnet_janky.py
@@ -0,0 +1,207 @@
+import numpy
+from gnuradio import gr
+
+
+class smartnet_janky(gr.sync_block):
+    PREAMBLE = [1,0,1,0,1,1,0,0]
+    PREAMBLE_LEN = len(PREAMBLE)
+    FRAME_LEN = 84
+    BODY_LEN = FRAME_LEN - PREAMBLE_LEN
+
+    def __init__(self, t0=0, packet_cb=None, skipped_cb=None, cksum_err_cb=None):
+        gr.sync_block.__init__(self, "smartnet_janky",
+                                in_sig=[numpy.byte],
+                                out_sig=None)
+
+        self.set_history(self.FRAME_LEN)
+        self.t0 = t0
+        self.nsamples = 0
+
+        self.packet_cb = packet_cb
+        self.skipped_cb = skipped_cb
+        self.cksum_err_cb = cksum_err_cb
+
+    def _find_preamble(self, a):
+        for i in range(len(a) - len(self.PREAMBLE)):
+            found_preamble = True
+            for j, bp in enumerate(self.PREAMBLE):
+                if bp != a[i+j]:
+                    found_preamble = False
+                    # print i, bp
+                    break
+
+            if found_preamble:
+                return i
+
+        return None
+                    
+    def _deinterleave(self, pkt_in):
+        # print "Deint: %s" % "".join(map(str, pkt_in))
+        retval = [0] * self.BODY_LEN
+        for i in range(self.BODY_LEN/4):
+            for j in range(4):
+                retval[i*4+j] = pkt_in[i+j*19]
+
+                # print "Deint= %s" % "".join(map(str, retval))
+        return retval
+
+    def _syndrome_check(self, frame):
+        l = len(frame)
+        i = [ frame[j] for j in range(0, l, 2) ]
+        k = [ frame[j] for j in range(1, l, 2) ]
+        errors = [ 0^i[0]^k[0] ] + [ k[1+j]^i[j]^i[j+1] for j in range(l/2-1) ]
+
+        return i,k,errors
+
+
+    def _ecc(self, frame):
+        l = len(frame)
+        i,k,errors = self._syndrome_check(frame)
+
+        #print i
+        #print k
+        #print errors
+
+        flips = 0
+
+        for j in range(len(errors)-1):
+            if errors[j]:
+                if errors[j+1]:
+                    # print "Flipping info %d" % j
+                    i[j] ^= 1
+                    errors[j+1] = 0
+                    flips += 1
+                else:
+                    #      print "Flipping parity %d" % j
+                    k[j] ^= 1
+                    flips += 1
+
+        if errors[-1]:
+            k[-1] ^= 1
+
+        errors = [ 0^i[0]^k[0] ] + [ k[1+j]^i[j]^i[j+1] for j in range(l/2-1) ]
+
+        return i,k,errors, flips
+
+    def _deframe(self, pkt_in):
+        i,k,errors, flips = self._ecc(self._deinterleave(pkt_in[self.PREAMBLE_LEN:]))
+
+        # print flips
+        if numpy.sum(errors):
+            #print self._ecc(pkt_in)
+            #print "Unresolved errors in packet."
+            return None
+
+        return i
+
+    def _crc(self, p):
+        crcaccum = 0x0393
+        crcop = 0x036e
+        for b in p[:27]:
+            if crcop & 1:
+                crcop = (crcop >> 1) ^ 0x0225
+            else:
+                crcop = (crcop >> 1)
+
+            if int(b) & 1:
+                crcaccum ^= crcop
+
+        return crcaccum
+
+
+    def _parse(self, pkt_in):
+        # print "".join(map(str, pkt_in[:self.FRAME_LEN]))
+
+        pkt = self._deframe(pkt_in)
+
+        if None == pkt:
+            return None
+
+            # print "DeECC: %s" % "".join(map(str, pkt))
+
+        def _fetch(i, n, invert=1):
+            retval = 0
+            for j in range(n):
+                retval *= 2
+                p_ij = int(pkt[i+j])
+                retval += invert^(1&p_ij)
+                # print "_fetch(%d,%d,%d): %d / %d => %d" % (i, n, invert, j, p_ij, retval)
+            return i+n, retval
+
+        cursor = 0
+        cursor, idno = _fetch(cursor, 16)
+        cursor, group = _fetch(cursor, 1)
+        cursor, cmd = _fetch(cursor, 10)
+        cursor, cksum = _fetch(cursor, 10)
+
+        cksum_expected = self._crc(pkt)
+
+
+        cmd ^= 0x32A
+        idno ^= 0x33C7
+
+        return { "cmd": "%x" % cmd, "group": group, "idno": idno, "cksum": cksum, "cksum_e": cksum_expected }
+
+
+
+
+    def work(self, input_items, output_items):
+        inbuf = input_items[0]
+        # print len(inbuf)
+        # print inbuf[:80]
+
+        preamble_at = self._find_preamble(inbuf)
+
+        if None == preamble_at:
+            self.nsamples += len(inbuf)
+            if self.skipped_cb:
+                self.skipped_cb(len(inbuf))
+            return len(inbuf)
+        timestamp = self.t0 + (self.nsamples+preamble_at)/3600.0
+
+        # print preamble_at, len(inbuf)
+
+        if preamble_at is None:
+            self.nsamples += len(inbuf)
+            return len(inbuf)
+
+        if 0 == preamble_at:
+            # print "ZERO: %d: %s" % (len(inbuf), "".join(map(str, inbuf[:8])))
+            g = self._parse(inbuf[:self.FRAME_LEN])
+
+            if g is None:
+                print "BOGON: " + "".join(map(str, inbuf[:self.FRAME_LEN]))
+
+            if g is not None and g["cksum"] == g["cksum_e"]:
+                if self.packet_cb:
+                    self.packet_cb(g)
+
+                #print str(g)
+                self.nsamples += self.FRAME_LEN
+                return self.FRAME_LEN
+            else:
+                #print "\tBogus checksum: %x / %x / %x" % (g["cksum"], g["cksum_e"], g["cksum"] ^ g["cksum_e"])
+                if self.cksum_err_cb:
+                    self.cksum_err_cb(g)
+                #print g
+                self.nsamples += 1
+                return 1
+
+        else:
+            self.nsamples += preamble_at
+            if self.skipped_cb:
+                self.skipped_cb(preamble_at)
+            # print "\tSkipped: %d" % preamble_at
+            return preamble_at
+
+
+def smartnet_pkt_print(g):
+    raw_addr = g["idno"]
+    talkgroup = raw_addr & ~0xF
+    flags = raw_addr & 0xF
+    freq = 0
+    cmd = int(g["cmd"], 16)
+    if cmd < 0x2d0:
+        freq = 851.0125 + 0.025*cmd
+
+    print "%d CC id=%s / tg=%d => cmd=%s / g=%s / f=%x / freq=%f" % (timestamp, str(g["idno"]), talkgroup, str(g["cmd"]), str(g["group"]), flags, freq)
--- scanner/split.py
+++ scanner/split.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2top
+#!/usr/bin/env python
 
 # Audio extraction/split functionality
 #
@@ -24,6 +24,7 @@
 
 from gnuradio import blocks
 from gnuradio import analog
+from gnuradio import digital
 from gnuradio import filter as gr_filter
 from gnuradio import eng_notation
 from gnuradio import gr
@@ -43,9 +44,122 @@ import os
 from timestamp_file_sink import timestamp_file_sink
 from time_trigger import time_trigger
 
-class recording_channelizer(gr.top_block):
 
-    def __init__(self, channels=None, samp_rate=2400000, Fc=852700000, base_url=None, threshold=-50):
+from smartnet_janky import *
+
+class recording_channelizer(gr.top_block):
+    def attach_audio_channel(self, f_i, i):
+        chain = []
+
+        pwr_squelch = analog.pwr_squelch_cc(self.threshold, 0.0001, 0, True)
+        nbfm_rx = analog.nbfm_rx(
+            audio_rate=self.chan_rate,
+            quad_rate=self.chan_rate,
+            tau=75e-6,
+            max_dev=5e3,
+        )
+
+
+        self.connect((self.pfb_channelizer_ccf_0, i),
+                     pwr_squelch, nbfm_rx)
+
+        return nbfm_rx
+
+    def attach_voice_finals(self, f_i, audio_source):
+        rational_resampler = gr_filter.rational_resampler_fff(
+            interpolation=16,
+            decimation=25,
+            taps=None,
+            fractional_bw=None,
+        )
+
+        scanpath = "scanner/%d/" % self.run_time
+
+        print "#"
+        print "# Output dir: %s" % scanpath
+        print "#"
+
+        try:
+            os.makedirs(scanpath)
+        except:
+            pass
+
+        f_bias = blocks.add_const_ff(1.0)
+        f_scale = blocks.multiply_const_ff(125.0)
+        f_to_char = blocks.float_to_uchar()
+
+
+        self.connect(audio_source,
+                     rational_resampler,
+                     f_bias,f_scale,f_to_char)
+
+
+        pattern = "%s/audio_%d_%%s.wav" % (scanpath, f_i)
+
+        wav_header = wave_header(1, 8000, 8, 0)
+
+        def wave_fixup_cb(fd, n_samples):
+            wave_fixup_length(fd)
+
+
+        def started_cb(x):
+            # print "Start: %s" % str(x)
+            unirest.get(self.base_url + "%d-start/1" % x, callback=lambda r: "Isn't that nice.")
+
+        def stop_cb(x):
+            # print " Stop: %s" % str(x)
+            unirest.get(self.base_url + "%d-stop/1" % x, callback=lambda r: "Isn't that nice.")
+
+
+
+        ttrig = time_trigger(self.holdoff, started_cb, stop_cb, f_i)
+        self.time_triggers.append(ttrig)
+
+
+        afile_sink = timestamp_file_sink(pattern,
+                                         self.holdoff,
+                                         header=wav_header,
+                                         final_cb=wave_fixup_cb)
+
+        self.connect(f_to_char, afile_sink)
+        self.connect(f_to_char, ttrig)
+
+
+
+    def attach_control_finals(self, f_i, audio_source):
+        print "attach control finals: %d" % f_i
+        rational_resampler = gr_filter.rational_resampler_fff(
+            interpolation=36,
+            decimation=125,
+            taps=None,
+            fractional_bw=None,
+        )
+
+
+        slicer = digital.binary_slicer_fb()
+
+        def print_pkt(s):
+            cmd = int(s["cmd"], 16)
+            if 0x2d0 >= cmd:
+                tunefreq = 851012500 + 25000*cmd
+                if tunefreq not in self.channels:
+                    print "UNK** %s" % str(s)
+                else:
+                    print "tun: %s" % str(s)
+
+
+        def print_skipped(n):
+            return # print "skip: %f / %d" % (time.time(), n)
+
+        def print_cksum_err(s):
+            print "   ** err: %s" % str(s)
+
+        snj = smartnet_janky(time.time(), print_pkt, print_skipped, print_cksum_err)
+
+        print "Control set up"
+        self.connect(audio_source, rational_resampler, slicer, snj)
+
+    def __init__(self, channels=None, samp_rate=2400000, Fc=852700000, base_url=None, threshold=-50, correction=0):
         gr.top_block.__init__(self, "Splitter")
 
 
@@ -55,22 +169,24 @@ class recording_channelizer(gr.top_block):
         self.samp_rate = samp_rate
         self.chan_rate = chan_rate = 12500
         self.n_channels = n_channels = samp_rate/chan_rate
-        
+
+
+        self.channels = channels
+
         self.Fc = Fc
 
         self.holdoff = holdoff = 0.5
         self.threshold = threshold # = -50.0
 
-        
         self.base_url = base_url
 
 
         self.gain = 10     # TODO expose gain settings at CLI
         self.gain_if = 20
         self.gain_bb = 20
-        
-        self.freq_corr = -1 # TODO expose frequency correction at CLI
-        
+
+        self.freq_corr = correction
+
         def channelizer_frequency(i):
             if i > n_channels/2:
                 return Fc - (n_channels - i)*chan_rate
@@ -103,15 +219,15 @@ class recording_channelizer(gr.top_block):
         print "#"
 
 
-        self.lpf_taps = lpf_taps = firdes.low_pass(1.0, 
-                                                   samp_rate, 
-                                                   samp_rate/n_channels/2, 
-                                                   samp_rate/n_channels/4, 
-                                                   firdes.WIN_HAMMING, 
+        self.lpf_taps = lpf_taps = firdes.low_pass(1.0,
+                                                   samp_rate,
+                                                   samp_rate/n_channels/2,
+                                                   samp_rate/n_channels/4,
+                                                   firdes.WIN_HAMMING,
                                                    6.76)
 
         print "# LPF taps: %d long" % len(self.lpf_taps)
-          
+
 
         ##################################################
         # Blocks
@@ -145,9 +261,9 @@ class recording_channelizer(gr.top_block):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.osmosdr_source_0, 0), (self.pfb_channelizer_ccf_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.pfb_channelizer_ccf_0, 0))
 
-        run_time = time.time()
+        self.run_time = time.time()
 
         self.time_triggers = []
 
@@ -156,86 +272,12 @@ class recording_channelizer(gr.top_block):
             f_i = channelizer_frequency(i)
 
             if None == channels or f_i in channels:
-                chain = []
+                audio_source = self.attach_audio_channel(f_i, i)
+                self.attach_voice_finals(f_i, audio_source)
 
-                pwr_squelch = analog.pwr_squelch_cc(self.threshold, 0.0001, 0, True)
+                if f_i == 851400000:
+                    self.attach_control_finals(f_i, audio_source)
 
-                self.connect((self.pfb_channelizer_ccf_0, i), 
-                             pwr_squelch)
-
-                nbfm_rx = analog.nbfm_rx(
-                    audio_rate=chan_rate,
-                    quad_rate=chan_rate,
-                    tau=75e-6,
-                    max_dev=5e3,
-                )
-
-                rational_resampler = gr_filter.rational_resampler_fff(
-                    interpolation=16,
-                    decimation=25,
-                    taps=None,
-                    fractional_bw=None,
-                )
-
-                scanpath = "scanner/%d/" % run_time
-
-                print "#"
-                print "# Output dir: %s" % scanpath
-                print "#"
-
-                try:
-                    os.makedirs(scanpath)
-                except:
-                    pass
-
-
-                f_bias = blocks.add_const_ff(1.0)
-                f_scale = blocks.multiply_const_ff(125.0)
-                f_to_char = blocks.float_to_uchar()
-
-                pattern = "%s/audio_%d_%%s.wav" % (scanpath, f_i)
-
-                wav_header = wave_header(1, 8000, 8, 0)
-
-                def wave_fixup_cb(fd, n_samples):
-                    wave_fixup_length(fd)
-
-
-                def started_cb(x):
-                    # print "Start: %s" % str(x)
-                    unirest.get(base_url + "%d-start/1" % x, callback=lambda r: "Isn't that nice.")
-
-                def stop_cb(x):
-                    # print " Stop: %s" % str(x)
-                    unirest.get(base_url + "%d-stop/1" % x, callback=lambda r: "Isn't that nice.")
-
-
-
-                ttrig = time_trigger(self.holdoff, started_cb, stop_cb, f_i)
-                self.time_triggers.append(ttrig)
-                                     
-
-                afile_sink = timestamp_file_sink(pattern, 
-                                                 self.holdoff, 
-                                                 header=wav_header, 
-                                                 final_cb=wave_fixup_cb)
-
-                self.connect(pwr_squelch,
-                             nbfm_rx,
-                             rational_resampler,
-                             f_bias,f_scale,f_to_char)
-
-                self.connect(f_to_char, afile_sink)
-                self.connect(f_to_char, ttrig)
-                                             
-
-                if False:
-                    file_sink = blocks.file_sink(gr.sizeof_gr_complex, 
-                                                 "%s/samples_%d.sdr" % (scanpath,f_i),
-                                                 True)
-                    self.connect(pwr_squelch, file_sink)
-
-                
             else:
                 null_sink = blocks.null_sink(gr.sizeof_gr_complex)
                 self.connect((self.pfb_channelizer_ccf_0, i), null_sink)
@@ -273,12 +315,12 @@ class recording_channelizer(gr.top_block):
 if __name__ == '__main__':
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
     parser.add_option("-c", "--channels", help="File containing channels list: will only emit these")
-    parser.add_option("-r", "--samp-rate", help="Sample rate (Hz)", default=2400000)
-    parser.add_option("-f", "--freq", help="Center frequency (Hz)", default=852700000)
+    parser.add_option("-r", "--samp-rate", help="Sample rate (Hz)", default=2400000, type=int)
+    parser.add_option("-f", "--freq", help="Center frequency (Hz)", default=852700000, type=int)
     parser.add_option("-u", "--url", help="Server base URL", default=None)
-    parser.add_option("-t", "--threshold", help="Squelch threshold on audio channels, dB", default=-50)
+    parser.add_option("-t", "--threshold", help="Squelch threshold on audio channels, dB", default=-50, type=int)
+    parser.add_option("-o", "--correction", help="Correction offset, PPM", default=0, type=int)
 
-    
     (options, args) = parser.parse_args()
 
 
@@ -289,7 +331,7 @@ if __name__ == '__main__':
         channels = map(int, filter(None, lines))
         f.close()
 
-    tb = recording_channelizer(channels, int(options.samp_rate), int(options.freq), options.url, int(options.threshold))
+    tb = recording_channelizer(channels, options.samp_rate, options.freq, options.url, options.threshold, options.correction)
     tb.start()
 
     while True:
